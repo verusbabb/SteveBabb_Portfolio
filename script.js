eb8e044b3a78d505d7853139378629f9fb5f88c2
Steve Babb <steve.babb@outlook.com>
2021-04-01 17:35:44 -0500

    update content and styling

--- script.js
+++ script.js
@@ -2,3 +2,13 @@
   $(document).ready(function(){
     $('.sidenav').sidenav();
   });
+
+  $(function() {
+    $('.card').hover(
+        function() {
+            $(this).find('> .card-image > img.activator').click();
+        }, function() {
+            $(this).find('> .card-reveal > .card-title').click();
+        }
+    );
+});
